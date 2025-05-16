2a34fd0a09cc5a75b8edacd3b477e94de104db46
Võ Hoàng Phúc <contact@vohoangphuc.com>
2025-05-16 13:06:49 +0700

    chore: update internal app domains and adjust platform URLs

--- packages/utils/src/internal-domains.ts
+++ packages/utils/src/internal-domains.ts
@@ -1,46 +1,14 @@
 export const PRODUCTION_INTERNAL_APP_DOMAINS = [
   {
-    name: 'platform',
-    url: 'https://tuturuuu.com',
-  },
-  {
-    name: 'calendar',
-    url: 'https://calendar.tuturuuu.com',
-  },
-  {
-    name: 'nova',
-    url: 'https://nova.tuturuuu.com',
-  },
-  {
-    name: 'nova',
-    url: 'https://nova.ai.vn',
-  },
-  {
-    name: 'mira',
-    url: 'https://mira.tuturuuu.com',
-  },
-  {
-    name: 'rewise',
-    url: 'https://rewise.me',
+    name: "platform",
+    url: "https://smartargi.ai",
   },
 ] as const;
 
 export const DEV_INTERNAL_APP_DOMAINS = [
   {
-    name: 'calendar',
-    url: 'http://localhost:7001',
-  },
-  {
-    name: 'platform',
-    url: 'http://localhost:7803',
-  },
-  {
-    name: 'rewise',
-    url: 'http://localhost:7804',
-  },
-  {
-    name: 'nova',
-    url: 'http://localhost:7805',
+    name: "platform",
+    url: "http://localhost:7803",
   },
 ] as const;
 
@@ -49,7 +17,7 @@ export const APP_DOMAIN_MAP = [
   ...DEV_INTERNAL_APP_DOMAINS,
 ] as const;
 
-export type AppName = (typeof APP_DOMAIN_MAP)[number]['name'];
+export type AppName = (typeof APP_DOMAIN_MAP)[number]["name"];
 
 export const INTERNAL_DOMAINS = [
   ...PRODUCTION_INTERNAL_APP_DOMAINS,
